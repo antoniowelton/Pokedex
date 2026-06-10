a9876381408fa679fe7195d5b7d762d9b06c8b36
antonio welton <antonioweltondev@gmail.com>
2026-06-10 16:03:27 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
-console.log('Sucesso!')
+
+const offset = 0 /* Configurando o 'passo' de apresentação dos pokemons */
+
+const limit = 10 // Configurando a quantidade máxima de pokemons a serem exibidos na tela 
+
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.log(error)) // Função que mostra um erro em caso de haver erro na requisição.
+        
+    
+
