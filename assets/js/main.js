b1f3fa13162b838a798abdb1a0a73bc50d5cf439
antonio welton <antonioweltondev@gmail.com>
2026-06-10 21:39:55 -0300

    Requisição de Api criada

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,10 +5,35 @@ const limit = 10 // Configurando a quantidade máxima de pokemons a serem exibid
 
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
-    .catch((error) => console.log(error)) // Função que mostra um erro em caso de haver erro na requisição.
+function convertPokemonToHtml(pokemon) { // Função que converte os dados do pokemon pra HTML, substituindo pelo nome de cada pokemon
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span> 
+                
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+                    </ol>
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                </div>
+        </li>  
+        `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons) => {
+    const listItens = []
+
+        for (let i = 1; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            listItens.push(convertPokemonToHtml(pokemon))
+        }    
+        
+        console.log(listItens)
+    }) 
         
     
 
